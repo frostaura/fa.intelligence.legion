065687352a6d06c8563c9417923e3ffb643ab263
Dean Martin <dean.martin@frostaura.net>
2023-05-24 00:56:27 +0200

    feat(Refactor): Migration from Zeus to Iluvatar mark 1.

---
+++ src/FrostAura.Intelligence.Iluvatar.Core/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FrostAura.Intelligence.Iluvatar.Shared.Extensions
+{
+	/// <summary>
+	/// Service collection extensions.
+	/// </summary>
+	public static class IServiceCollectionExtensions
+	{
+        /// <summary>
+        /// Bind the required configuration to the DI service collection.
+        /// </summary>
+        /// <param name="serviceCollection">The DI service collection.</param>
+        /// <returns>The DI service collection.</returns>
+        public static IServiceCollection AddSharedConfiguration(this IServiceCollection serviceCollection, IConfigurationRoot configuration)
+		{
+			return serviceCollection;
+        }
+
+        /// <summary>
+        /// Register shared services to the DI service collection.
+        /// </summary>
+        /// <param name="serviceCollection">The DI service collection.</param>
+        /// <returns>The DI service collection.</returns>
+        public static IServiceCollection AddSharedServices(this IServiceCollection serviceCollection, IConfigurationRoot configuration)
+        {
+            return serviceCollection;
+        }
+    }
+}
---
+++ src/FrostAura.Intelligence.Iluvatar.Shared/Models/Config/PineconeConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace FrostAura.Intelligence.Iluvatar.Shared.Models.Config
+{
+    /// <summary>
+    /// The OpenAI API configuration model.
+    /// </summary>
+    [DebuggerDisplay("{ApiEndpoint}")]
+	public class OpenAIConfig
+	{
+		/// <summary>
+		/// The API endpoint to use as the root.
+		/// </summary>
+		public string ApiEndpoint { get; set; }
+        /// <summary>
+        /// The API key to use to auth.
+        /// </summary>
+        public string ApiKey { get; set; }
+        /// <summary>
+        /// The model name of the completion model to use.
+        /// </summary>
+        public string ModelName { get; set; }
+        /// <summary>
+        /// The model name of the embedding model to use.
+        /// </summary>
+        public string EmbeddingModelName { get; set; }
+    }
+}
